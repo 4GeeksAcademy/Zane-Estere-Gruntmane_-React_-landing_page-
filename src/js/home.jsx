7325541181b092c0db421f997727c0f22accea87
Zane Estere Gruntmane <141255754+esterzane@users.noreply.github.com>
2023-11-07 14:49:09 +0000

    new images updated, missing the last one about future universe

--- src/js/home.jsx
+++ src/js/home.jsx
@@ -18,12 +18,12 @@ const Home = () => {
 			description="Why do the X-Men always excel in spelling bees? Because they have a real Magneto when it comes to finding the right words!"
 			/>
 			<Card
-			imageUrl="https://pub-3626123a908346a7a8be8d9295f44e26.r2.dev/generations/545a20ba-bc78-4a39-9eb7-790c2eb64919-0.png"
+			imageUrl="https://chat-gpt.photos/image-proxy/35581523-cab8-4a72-971c-24cc80eb743a-0.png"
 			title="The Infinity Saga"
 			description="Why did Marvel's Infinity Saga feel like the longest road trip ever? Because they took more detours than a GPS with a sense of adventure!"
 			/>
 			<Card
-			imageUrl="https://pub-3626123a908346a7a8be8d9295f44e26.r2.dev/generations/545a20ba-bc78-4a39-9eb7-790c2eb64919-0.png"
+			imageUrl="https://chat-gpt.photos/image-proxy/aed9ca4e-80a1-43c8-b917-b51dc8575a3c-2.png"
 			title="The Multiverse Saga"
 			description="I am Groot! Translating - In the Multiverse Saga, it's like a forest of possibilities, but with more capes and fewer leaves!"
 			/>
